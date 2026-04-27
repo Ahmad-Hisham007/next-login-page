348f8b4f890a512b99037259289bdb53842ad490
Ahmad Hisham <ahhisham413@gmail.com>
2026-04-27 15:09:43 +0600

    feat(database): This commit will add 3 new models to Mongoose schema models. 3 models for 3 new collections will come from NextAuth

---
+++ models/User.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const UsersSchema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String, unique: true, required: true },
+  emailVerified: { type: Date, default: null },
+  image: { type: String },
+  // Extra fields
+  password: { type: String },
+  role: { type: String, default: "user" },
+});
+const User = mongoose.models.User || mongoose.model("User", UsersSchema);
+
+export default User;
